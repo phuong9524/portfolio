4c12fc0a39a6dd273fbc153b8d202b04b4ddb8a0
Do Nguyen Phuong <43663068+phuong9524@users.noreply.github.com>
2023-09-21 15:18:52 +0700

    Add link to card project and animation icon. Remove comment in tailwind config

--- src/app/components/card-project.jsx
+++ src/app/components/card-project.jsx
@@ -5,7 +5,7 @@ export default function CardProject(props) {
   const { img, title, link, describe, skills } = props;
 
   return (
-    <div className="flex gap-5 flex-col md:flex-row mb-12">
+    <div className="flex gap-5 flex-col md:flex-row mb-12 ">
       <div className="w-[22%]">
         <Image
           className="rounded border-2 border-t-slate-200/10 transition sm:order-last"
@@ -18,8 +18,27 @@ export default function CardProject(props) {
         />
       </div>
       <div className="w-[100%] flex flex-col gap-4">
-        <a href={link}>
+        <a
+          target="_blank"
+          rel="noreferrer noopener"
+          href={link}
+          className="group inline-flex gap-2 fill-current  hover:text-violet-500 items-baseline "
+        >
           <span>{title}</span>
+
+          <div>
+            <span className="bg-gradient-to-r from-indigo-500 to-violet-500 invisible blur-md absolute rounded-lg group-hover:visible text-sm transition group-hover:translate-x-1 group-hover:-translate-y-1">
+              ---
+            </span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="0.8em"
+              viewBox="0 0 512 512"
+              className="group-hover:transition group-hover:translate-x-2 group-hover:-translate-y-1"
+            >
+              <path d="M156.6 384.9L125.7 354c-8.5-8.5-11.5-20.8-7.7-32.2c3-8.9 7-20.5 11.8-33.8L24 288c-8.6 0-16.6-4.6-20.9-12.1s-4.2-16.7 .2-24.1l52.5-88.5c13-21.9 36.5-35.3 61.9-35.3l82.3 0c2.4-4 4.8-7.7 7.2-11.3C289.1-4.1 411.1-8.1 483.9 5.3c11.6 2.1 20.6 11.2 22.8 22.8c13.4 72.9 9.3 194.8-111.4 276.7c-3.5 2.4-7.3 4.8-11.3 7.2v82.3c0 25.4-13.4 49-35.3 61.9l-88.5 52.5c-7.4 4.4-16.6 4.5-24.1 .2s-12.1-12.2-12.1-20.9V380.8c-14.1 4.9-26.4 8.9-35.7 11.9c-11.2 3.6-23.4 .5-31.8-7.8zM384 168a40 40 0 1 0 0-80 40 40 0 1 0 0 80z" />
+            </svg>
+          </div>
         </a>
         <p className="text-sm leading-normal text-slate-400">{describe}</p>
         <div className="flex gap-2 flex-wrap justify-start">
